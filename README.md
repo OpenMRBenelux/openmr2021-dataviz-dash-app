8adbfd9cf79e1fd4e3eb0a84d83ce46761e34775
jsheunis <jsheunis@gmail.com>
2021-03-25 12:46:36 +0100

    app feature additions

--- README.md
+++ README.md
@@ -1,9 +1,40 @@
-# openmr2021-dataviz-dash-app
+# Interactive visualization with Python, Plotly and Dash - Part 2
 
+This repository contains the content of Part 2 of the "Interactive visualization with Python, Plotly and Dash" demo presented at the [OpenMR Virtual 2021](https://openmrbenelux.github.io/) event:
 
+> The tools are out there for researchers to publish their data and results in rich online formats that offer accessibility, interaction and exploration. Yet, we still see a majority of standard plots and images in PDF format. In this demo, we will explore the possibilities offered by Python, Plotly and Dash to make your research outputs interactive and future-aware!
 
+## Content
 
-## Create required application files
+The whole demo follows [this presentation](https://tinyurl.com/openmr-dash-demo). The first part runs through content that can be accessed at this repository: [https://github.com/OpenMRBenelux/openmr2021-dataviz-dash-demo](https://github.com/OpenMRBenelux/openmr2021-dataviz-dash-demo).
+
+In this second part of the demonstration, you are given a walkthrough of how to create and deploy an interactive Dash app.
+
+You can access the resulting sample application here: [https://openmr2021-dash-demo.herokuapp.com/](https://openmr2021-dash-demo.herokuapp.com/).
+
+## Instructions
+
+If you would like to run through the setup and deployment of a Dash app from your own machine, you can follow the instructions below.
+
+### Create a Python environment
+
+First, you need to have Python installed, after which you should set up a virtual environment in which to install all the required dependencies. For example, you can use install and use miniconda for virtual environment management. Once installed, create the environment:
+
+`conda create -n <YOUR-ENV-NAME> python`
+
+Then activate the environment:
+
+`conda activate <YOUR-ENV-NAME>`
+
+Then install all dependencies:
+
+`pip install -r requirements.txt`
+
+### Create a git repository
+
+Either by creating one on Github and subsequently cloning it to your device, or via the command line.
+
+### Create required application files
 
 1. `requirements.txt`
 2. `app.py`
@@ -12,41 +43,40 @@
 5. (optional) `assets` and/or `data` directories to store any content used by the application
 
 
-## Deploy your app to Heroku
+### Deploy your app to Heroku
 
-#### 1. Create a free Heroku account
+##### 1. Create a free Heroku account
 
 This is the service with which we will deploy our Dash application. Visit [Heroku](https://www.heroku.com/) to create a free account.
 
-#### 2. Install the Heroku CLI
+##### 2. Install the Heroku CLI
 
 The [Heroku CLI](https://devcenter.heroku.com/articles/heroku-cli) needs to be installed locally in order to interact with Heroku via the command line.
 
-#### 3. Log in to Heroku via the command line
+##### 3. Log in to Heroku via the command line
 
 `heroku login`
 
 This will prompt you to press any key, after which it will open a browser window where you will log in with your Heroku credentials. After doing this, return to the comman line.
 
-#### 4. Create a new Heroku app
+##### 4. Create a new Heroku app
 
 `heroku create -n <YOUR-APP-NAME>`
 
 where `<YOUR-APP-NAME>` refers to the preferred title for your new Dash app. This will return a URL in the form `https://git.heroku.com/<YOUR-APP-NAME>.git`
 
-
-#### 5. Set the Heroku remote
+##### 5. Set the Heroku remote
 
 `heroku git:remote -a <YOUR-APP-NAME>`
 
 This connects your local code base with the remote Heroku app that you just created.
 
-#### 6. Push your changes to the remote Heroku repository
+##### 6. Push your changes to the remote Heroku repository
 
 `git push heroku main`
 
 This deploys your app to Heroku!
 
-#### 7. Create a Dyno and make your app live
+##### 7. Create a Dyno and make your app live
 
 `heroku ps:scale web=1`
--- app.py
+++ app.py
@@ -4,42 +4,33 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
+import plotly.graph_objs as go
+import os
+import pandas as pd
+import numpy as np
 
-# ---------------
-# Style sheets
-# ---------------
-# external_stylesheets = [
-#     'https://codepen.io/chriddyp/pen/bWLwgP.css',
-#     {
-#         'href': 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
-#         'rel': 'stylesheet',
-#         'integrity': 'sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO',
-#         'crossorigin': 'anonymous'
-#     }
-# ]
 
 # ---------------
 # Prepare data
 # ---------------
 
-# img = datasets.fetch_localizer_button_task()['tmap']     
-# view = plotting.view_img_on_surf(img, threshold='90%', surf_mesh='fsaverage')
-# # view.save_as_html("pic1.html") 
+# Grab data from CSV file and divide into arrays
+data_dir = 'data'
+tsnr_csv = os.path.join(data_dir, 'tsnr.csv')
+df_tsnr = pd.read_csv(tsnr_csv)
+dat1 = df_tsnr['brain'].dropna().to_numpy()
+dat2 = df_tsnr['gm'].dropna().to_numpy()
+dat3 = df_tsnr['wm'].dropna().to_numpy()
+dat4 = df_tsnr['csf'].dropna().to_numpy()
 
-main_md = dcc.Markdown('''The tools are out there for researchers to publish their data and results in rich online formats that offer accessibility, interaction and exploration. Yet, we still see a majority of standard plots and images in PDF format. In this demo, we will explore the possibilities offered by Python, Plotly and Dash to make your research outputs interactive and future-aware!''')
-# Create card components to display on home page
-card_access = [
-    dbc.CardBody(
-        [
-            html.H5("Access", className="card-title"),
-            html.P(
-                "The BIDS-compatible rt-me-fMRI dataset is accessible via a GDPR-compliant data use agreement on DataverseNL...",
-                className="card-text",
-            ),
-            dbc.Button("Access the dataset", color="light", href="/pages/access", external_link=True),
-        ]
-    ),
-]
+# Create empty Plotly graph (updated in the relevant callback)
+fig3 = go.Figure()
+
+# Create options for dropdown
+all_options = ['box', 'violin']
+sub_opts = [{'label': opt, 'value': opt} for opt in all_options]
+
 
 # ---------------
 # Create app
@@ -48,7 +39,6 @@ card_access = [
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], meta_tags=[
                     {"name": "viewport", "content": "width=device-width, initial-scale=1"}
                 ])
-# app.config.suppress_callback_exceptions = True
 server = app.server
 
 
@@ -56,11 +46,14 @@ server = app.server
 # App layout
 # ---------------
 
-
+main_md = dcc.Markdown('''The tools are out there for researchers to publish their data and results in rich online formats that offer accessibility, interaction and exploration.
+Yet, we still see a majority of standard plots and images in PDF format.
+In this demo, we will explore the possibilities offered by Python, Plotly and Dash to make your research outputs interactive and future-aware!
+''')
 
 app.layout = html.Div([
     html.Div(
-        html.Img(src="/assets/section_header.png", width="100%"),
+        html.Img(src="/assets/section_header.png", width="70%"),
         style={ 'textAlign': 'center'}
     ),
     html.Br(),
@@ -71,7 +64,6 @@ app.layout = html.Div([
         }
     ),
     html.Br(),
-    
     html.Div([
         main_md,],
         style={
@@ -82,13 +74,45 @@ app.layout = html.Div([
     html.Br(),
     html.Div([
         dbc.Row([
-            dbc.Col(dbc.Card(card_access, color="secondary", inverse=True)),
-            dbc.Col(dbc.Card(card_access, color="info", inverse=True)),
-            ],
-            className="mb-4",
-        ),
-        ]
-    )
+            dbc.Col([
+                html.H4(
+                    children='Interactive tSNR plots',
+                    style={
+                        'textAlign': 'center',
+                    }
+                ),
+                html.Br(),
+                html.P('Select a type of chart:'),
+                dcc.Dropdown(
+                    id='drop_plot_options',
+                    options=sub_opts,
+                    value='box'
+                ),
+                html.Br(),
+                html.H6(
+                    children='Interactive tSNR chart',
+                    id='heading3',
+                    style={
+                        'textAlign': 'center',
+                    }
+                ),
+                dcc.Graph(figure=fig3, id='fig3')
+                ], width={"size": 6, "offset": 0}
+            ),
+            dbc.Col([
+                html.H4('Finger Tapping Task Map', style={'textAlign': 'center'}),
+                html.Br(),
+                html.Br(),
+                html.Br(),
+                html.Br(),
+                html.Br(),
+                html.H6('A motor contrast that is visible and interactively explorable in three dimensions', style={'textAlign': 'center'}),
+                html.Iframe(id='task_map1', src='/assets/statmap_viewer.html', style={'border': 'none', 'width': '100%', 'height': 500}),   
+                # html.Iframe(id='task_map2', src='/assets/surface_viewer.html', style={'border': 'none', 'width': '100%', 'height': 500}),   
+                ], width={"size": 6, "offset": 0}
+            ),
+        ]),
+    ])
 ],
 style={
     'marginBottom': 25,
@@ -97,22 +121,57 @@ style={
     'maxWidth': '90%'
 })
 
+
+
 # ---------------
 # App callbacks
 # ---------------
 
-# @app.callback(
-#     Output(component_id='my-output', component_property='children'),
-#     Input(component_id='my-input', component_property='value')
-# )
-# def update_output_div(input_value):
-#     return 'Output: {}'.format(input_value)
+# Callback for updating the box/violin plot
+@app.callback(
+    Output('fig3', 'figure'),
+    [Input('drop_plot_options', 'value')]
+)
+def update_plot(plot_option):
+    colors = ['#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#66a61e', '#e6ab02']
+    layout = go.Layout(
+        yaxis = dict(title = 'Temporal signal-to-noise ratio (tSNR)', range=[-20, 250]),
+        xaxis=dict(title='Tissue masks'),
+        margin={'t': 0},
+    )
+    fig3 = go.Figure(layout=layout)
+    if plot_option == 'box':
+        fig3.add_trace(go.Box(y=dat1, line_color=colors[0], name='Brain'))
+        fig3.add_trace(go.Box(y=dat2, line_color=colors[1], name='GM'))
+        fig3.add_trace(go.Box(y=dat3, line_color=colors[2], name='WM'))
+        fig3.add_trace(go.Box(y=dat4, line_color=colors[3], name='CSF'))
+        heading = 'koek'
+    else:
+        fig3.add_trace(go.Violin(y=dat1, line_color=colors[0], name='Brain'))
+        fig3.add_trace(go.Violin(y=dat2, line_color=colors[1], name='GM'))
+        fig3.add_trace(go.Violin(y=dat3, line_color=colors[2], name='WM'))
+        fig3.add_trace(go.Violin(y=dat4, line_color=colors[3], name='CSF'))
+        fig3.update_traces(orientation='v', side='positive', width=1, box_visible=True, points=False)
+        fig3.update_layout(xaxis_showgrid=True, yaxis_showgrid=True, xaxis_zeroline=False)
+    return fig3
+
+# Callback for updating heading based on selected
+@app.callback(
+     Output('heading3','children'),
+    [Input('fig3', 'clickData')]
+)
+def reset_sub_tsnr_info(clickData): 
+    if clickData is None:
+        raise PreventUpdate
+    else:
+        curves = ['Brain', 'GM', 'WM', 'CSF']
+        selected_trace = curves[clickData['points'][0]['curveNumber']]
+        new_heading = 'Interactive tSNR chart (selected data = ' + selected_trace + ')' 
+    return new_heading
+
 
 # ---------------
 # Run app
 # ---------------
-
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
